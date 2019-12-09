2f7b33e10d4233cf1f5f14e9316481f5927d2e7e
sumansumit <suman.sumit09@gmail.com>
2019-12-09 12:18:22 +0530

    American User

--- src/test/java/quickclickacademy/SeleniumTest.java
+++ src/test/java/quickclickacademy/SeleniumTest.java
@@ -4,11 +4,16 @@ public class SeleniumTest {
 	public void getDetails() {
 		System.out.println("Selenium UI Testing");
 	}	
+	
+	public void getName() {
+		System.out.println("UserName");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SeleniumTest st=new SeleniumTest();
 		st.getDetails();
+		st.getName();
 		
 		
 
